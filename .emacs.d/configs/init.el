f2d3effb20e5b423b49146d0392bc115ac78f66a
Andrew Kanner <andrew.kanner@gmail.com>
2015-01-20 23:20:52 +0300

    config-files renaming

--- .emacs.d/configs/init.el
+++ .emacs.d/configs/init.el
@@ -13,27 +13,31 @@
 ;; general settings
 ;;------------------------------------------------------------------------------
 
+;; path
 (defun add-path (path)
   (add-to-list 'load-path (concat custom-basedir path)))
 
+;; config loading function
 (defun load-my-cfg (file)
   (load-file (concat custom-basedir file)))
 
+;; config files list
 (setq my-cfg-list '(
-                    "my-general.el"
-                    "my-functions.el"
-                    "my-kbd-shortcuts.el"
-                    "my-spell.el"
-                    "my-auctex.el"
-                    "my-org-mode.el"
-                    "my-c-mode.el"
-                    "my-perl.el"
-                    "my-c++.el"
-                    "my-css-html.el"
-                    "my-eshell.el"
-                    "my-slime.el"
-                    "my-gnus.el"
-                    "my-elisp.el"
+		    "auctex.el"
+		    "c-mode.el"
+		    "c++-mode.el"
+		    "elisp.el"
+		    "eshell.el"
+;		    "functions.el"
+		    "general.el"
+;		    "gnus.el"
+		    "kbd-hotkeys.el"
+		    "org-mode.el"
+		    "perl-mode.el"
+		    "slime.el"
+		    "spell.el"
+		    "webdev-mode.el"
                     ))
 
+;; load all config files from list
 (mapc 'load-my-cfg my-cfg-list)
