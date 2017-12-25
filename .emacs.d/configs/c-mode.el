daa959e71560f204706d64c74ed24a6f9934ed68
Andrew Kanner <andrew.kanner@gmail.com>
2017-12-25 13:11:35 +0300

    c-mode.el: refactoring in order to support Linux kernel style

--- .emacs.d/configs/c-mode.el
+++ .emacs.d/configs/c-mode.el
@@ -1,15 +1,15 @@
-;;------------------------------------------------------------------------------
-;; C-Mode
-;;------------------------------------------------------------------------------
-
 (message "applying c-mode settings ...")
 
 (c-add-style
- "python-new"
- '((indent-tabs-mode . nil)
-   (fill-column      . 78)
+ "myC"
+   ;; insert tabs instead of spaces (". nil")
+ '((indent-tabs-mode t)
+   ;; 80 characters in line (maximum line width)
+   (fill-column      . 80)
    (c-toggle-auto-newline 0)
-   (c-basic-offset   . 4)
+   ; 8 spaces as a tab (kernel style)
+   (tab-width   . 8)
+   (c-basic-offset   . 8)
    (c-offsets-alist  . ((substatement-open . 0)
                         (inextern-lang . 0)
                         (arglist-intro . +)
@@ -22,31 +22,43 @@
                               (block-close . c-snug-do-while)))
    (c-block-comment-prefix . "* ")))
 
-(c-add-style
- "javascript"
- '((indent-tabs-mode . nil)
-   (fill-column      . 80)
-   (c-basic-offset   . 2)
-   (c-offsets-alist  . ((substatement-open . 0)
-                        (inextern-lang . 0)
-                        (arglist-intro . +)
-                        (knr-argdecl-intro . +)))
-   (c-hanging-braces-alist . ((brace-list-open)
-                              (brace-list-intro)
-                              (brace-list-close)
-                              (brace-entry-open)
-                              (substatement-open after)
-                              (block-close . c-snug-do-while)))
-   (c-block-comment-prefix . "")))
+(add-to-list 'c-default-style '(c-mode . "myC"))
 
-(add-to-list 'c-default-style '(c-mode . "python-new"))
-(add-to-list 'c-default-style '(ecmascript-mode . "javascript"))
+;(defun c-select-style ()
+;  "Select the C style to use from buffer indentation."
+;  (save-excursion
+;    (if (re-search-forward "^\t" 3000 t)
+;        (c-set-style "python")
+;      (c-set-style "myC"))))
+;(add-hook 'c-mode-hook 'c-select-style)
 
-(defun c-select-style ()
-  "Select the C style to use from buffer indentation."
-  (save-excursion
-    (if (re-search-forward "^\t" 3000 t)
-        (c-set-style "python")
-      (c-set-style "python-new"))))
+;; linux/Documentation/process/coding-style.rst
+(defun c-lineup-arglist-tabs-only (ignored)
+  "Line up argument lists by tabs, not spaces"
+  (let* ((anchor (c-langelem-pos c-syntactic-element))
+         (column (c-langelem-2nd-pos c-syntactic-element))
+         (offset (- (1+ column) anchor))
+         (steps (floor offset c-basic-offset)))
+    (* (max steps 1)
+       c-basic-offset)))
 
-(add-hook 'c-mode-hook 'c-select-style)
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            ;; Add kernel style
+            (c-add-style
+             "linux-tabs-only"
+             '("linux" (c-offsets-alist
+                        (arglist-cont-nonempty
+                         c-lineup-gcc-asm-reg
+                         c-lineup-arglist-tabs-only))))))
+
+(add-hook 'c-mode-hook
+          (lambda ()
+            (let ((filename (buffer-file-name)))
+              ;; Enable kernel mode for the appropriate files
+              (when (and filename
+                         (string-match (expand-file-name "~/projects/linux")
+                                       filename))
+                (setq indent-tabs-mode t)
+                (setq show-trailing-whitespace t)
+                (c-set-style "linux-tabs-only")))))
